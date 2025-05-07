841e1de2e9b58def980879cd0fdfafbdb5b6e83d
Max Görner <5477952+MaxG87@users.noreply.github.com>
2025-05-07 19:47:24 +0200

    feat: Extend deploy script to install for all kernels
    If, for whatever reason, this Kernel module is installed freshly, the
    existing deploy script would not install the module for all available
    Kernels. If a user would boot into another Kernel (e.g. 6.12.21-amd64
    instead of 6.12.22-amd64) Wi-Fi would not be available. This can be
    confusing and annoying.
    
    The changes of this commit would prune the module from DKMS and then
    install it for all available Kernels. This is much more convenient.

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,25 @@
 
 set -euo pipefail
 
+function main() {
+    local VERSION
+    VERSION="$(get_version)"
+    ensure_no_cli_args "$@"
+    ensure_root_permissions
+    remove_driver &>/dev/null || true
+    put_sources_in_place "$VERSION"
+    deploy_driver "$VERSION"
+}
+
 function ensure_no_cli_args() {
-    if [ $# -ne 0 ]
-    then
+    if [ $# -ne 0 ]; then
         echo "No command line arguments accepted!" >&2
         exit 1
     fi
 }
 
 function ensure_root_permissions() {
-    if ! sudo -v
-    then
+    if ! sudo -v; then
         echo "Root permissions required to deploy the driver!" >&2
         exit 1
     fi
@@ -22,16 +30,27 @@ function get_version() {
     sed -En 's/PACKAGE_VERSION="(.*)"/\1/p' dkms.conf
 }
 
+function remove_driver() {
+    sudo dkms remove rtl88x2bu/5.8.7.1 --all
+}
+
+function put_sources_in_place() {
+    local VERSION="$1"
+    sudo rsync --delete --exclude=.git -rvhP ./ "/usr/src/rtl88x2bu-${VERSION}"
+}
+
 function deploy_driver() {
-    VER=$(get_version)
-    sudo rsync --delete --exclude=.git -rvhP ./ "/usr/src/rtl88x2bu-${VER}"
-    for action in add build install
-    do
-      sudo dkms "${action}" -m rtl88x2bu -v "${VER}"
-    done
+    local VERSION="$1"
+    sudo dkms "add" -m rtl88x2bu -v "${VERSION}"
+    find /boot -maxdepth 1 -iname "initrd.img*" |
+        cut -d- -f2- |
+        while read -r kernel; do
+            # xargs -n1 sudo dkms install -m rtl88x2bu -v 5.8.7.1 -k
+            for action in build install; do
+                sudo dkms "${action}" -m rtl88x2bu -v "${VERSION}" -k "${kernel}"
+            done
+        done
     sudo modprobe 88x2bu
 }
 
-ensure_no_cli_args "$@"
-ensure_root_permissions
-deploy_driver
+main "$@"
